19ecc7c30d270b83345123e14577e3684a78ed69
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-05-15 14:55:24 -0300

    auditapath: setup besu node and hardhat deploy

--- auditapath/blockchain/hardhat.config.js
+++ auditapath/blockchain/hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    besu: {
+      url: "http://localhost:8545", 
+      accounts: ["0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"], 
+    }
+  },
   solidity: "0.8.28",
 };
