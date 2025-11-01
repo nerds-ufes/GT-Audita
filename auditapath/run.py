4a6e5b088cd402fb6269f05bfe3b6134a4577b3a
YuriVictoria <yuri.victoria@edu.ufes.br>
2025-11-01 00:25:10 -0300

    v2.1

--- auditapath/run.py
+++ auditapath/run.py
@@ -4,27 +4,27 @@ import polka_halfsiphash.script.scenarios as scenarios
 if __name__ == "__main__":
     
     topology_menu = """
-    *** (1)-Simple
-    *** (2)-Linear
-    """
+*** (1)-Simple
+*** (2)-Linear
+"""
 
     case_menu = """
-    *** (1)-Default
-    *** (2)-Addition
-    *** (3)-Partial Detour
-    *** (4)-Complete Detour
-    *** (5)-Skipping
-    *** (6)-Out of Order
-    """
+*** (1)-Default
+*** (2)-Addition
+*** (3)-Partial Detour
+*** (4)-Complete Detour
+*** (5)-Skipping
+*** (6)-Out of Order
+"""
 
-    topology = input(topology_menu + "\n    *** Topology: ")
+    topology = input(topology_menu + "\n--- Topology: ")
 
     if topology == "1":
         scenarios.simple()
     
     elif topology == "2":
     
-        case = input(case_menu + "\n*** Case: ")
+        case = input(case_menu + "\n--- Case: ")
 
         if case == "1":
             scenarios.default()
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             scenarios.outoforder()
 
         else:
-            print("Invalid case!\n")
+            print("*** Invalid case!\n")
     
     else:
-        print("Invalid topology!\n")
+        print("*** Invalid topology!\n")
